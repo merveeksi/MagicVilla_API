f4b14a1dfca23ec95ccf39e2533a3927038798fa
MERVE EKŞİ <merveeksii61@gmail.com>
2024-06-21 12:09:05 +0300

    repos

--- MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
+++ MagicVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -5,8 +5,8 @@ namespace MagicVilla_VillaAPI.Repository.IRepository
 {
     public interface IVillaRepository
     {
-        Task <List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
-        Task<List<Villa>> Get(Expression<Func<Villa>> filter = null, bool tracked=true);
+        Task <List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked=true);
         Task Create(Villa entity);
         Task Remove(Villa entity);
         Task Save();
--- MagicVilla_VillaAPI/Repository/IRepository/VillaRepository.cs
+++ MagicVilla_VillaAPI/Repository/IRepository/VillaRepository.cs
@@ -13,40 +13,45 @@ namespace MagicVilla_VillaAPI.Repository
         {
             _db = db;
         }
-
         public async Task Create(Villa entity)
         {
-            await _db.Villas.AddAsync(entity);
+            await _db.Villas.AddAsync (entity);
             await Save();
         }
 
-        //public Task Create(VillaRepository entity)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        public Task<Villa> Get(Expression<Func<Villa>> filter = null, bool tracked = true)
-        {
-            throw new NotImplementedException();
-        }
-        Task<List<Villa>> IVillaRepository.GetAll(Expression<Func<Villa>> filter)
+        public async Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
         {
             IQueryable<Villa> query = (IQueryable<Villa>)_db.Villas;
 
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+#pragma warning disable CS8603 // Possible null reference return.
+            return await query.FirstOrDefaultAsync();
+#pragma warning restore CS8603 // Possible null reference return.
         }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null)
+        {
+            IQueryable<Villa> query = (IQueryable<Villa>)_db.Villas;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
         public async Task Remove(Villa entity)
         {
             _db.Villas.Remove(entity);
             await Save();
         }
+
         public async Task Save()
         {
             await _db.SaveChangesAsync();
-        }
-
-        Task<List<Villa>> IVillaRepository.Get(Expression<Func<Villa>> filter, bool tracked)
-        {
-            throw new NotImplementedException();
         }
     }
 }
